1dba8b08b34853e12d51113d817336279e550d39
unknown <david.padilha@hotmail.com>
2020-12-06 13:17:42 -0300

    Alterações

--- cypress/support/commandasCONTA.js
+++ cypress/support/commandasCONTA.js
@@ -2,7 +2,7 @@ import loc from './locators'
 
 
 //COMO SE FOSSEM OS METODOS EM RUBY
-// Metodos é um consjunto de ações orientadas a objetos
+// Metodos são conjuntos de ações orientadas a objetos
 
 Cypress.Commands.add('acessarMenuConta', () =>{
     cy.get(loc.MENU.SETTINGS).click()
