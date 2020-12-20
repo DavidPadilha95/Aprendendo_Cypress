0439fa2afb05d9ee73d0736cf8bb5f5146304b59
unknown <david.padilha@hotmail.com>
2020-12-20 11:48:46 -0300

    Novos casos de testes para o backend

--- cypress/integration/Barriga/Backend.spec.js
+++ cypress/integration/Barriga/Backend.spec.js
@@ -2,22 +2,33 @@
 // .then(res => console.log(res)) //para apresentar no console o que foi encontrado
 
 describe('Testando a nivel funcional', () => {
-    before(() => { //Logando no usuario antes de tudo
-        // cy.login('david.cypress@teste.com', 'teste123')
-        // cy.validarMensagem('Bem vindo')
-        // cy.resetApp()
+
+    let token //Crei a variavel do token
+
+    before(() => { 
+        cy.getToken('david.cypress@teste.com', 'teste123')
+            .then( tkn=> {
+                token = tkn
+            }) //Fiz um then para quando pegar o token no login armazenar na variavel
         
     })
 
     it('Criando novas contas', () => {
-        cy.request({
-            method: 'POST',
-            url: 'https://barrigarest.wcaquino.me/signin',
-            body:{  email: "david.cypress@teste.com",
-                    redirecionar: false,
-                    senha: "teste123"}
-        }).its('body.token').should('not.be.empty')
-     
+                cy.request({
+                method: 'POST',
+                url: 'https://barrigarest.wcaquino.me/contas',
+                headers: {Authorization: `JWT ${token}`},
+                body: {
+                 nome: 'Conta via rest'
+                }
+            }).as('response')
+
+        cy.get('@response').then(res => {
+            expect(res.status).to.be.equal(201)
+            expect(res.body).to.have.property('id')
+            expect(res.body).to.have.property('nome', 'Conta via rest')
+        })
+
     })
 
     it('Editando uma conta', () => {
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -13,3 +13,18 @@ Cypress.Commands.add('resetApp', () =>{ //Comando para resetar as contas antes d
     cy.get(loc.MENU.SETTINGS).click()
     cy.get(loc.MENU.RESET).click()
 })
+
+Cypress.Commands.add('getToken',(user, passwd) =>{
+    cy.request({
+        method: 'POST',
+        url: 'https://barrigarest.wcaquino.me/signin',
+        body:{  
+            email: user,
+            redirecionar: false,
+            senha: passwd
+        }
+    }).its('body.token').should('not.be.empty')
+      .then(token => {
+            return token
+      }) //Esse é um teste que o token foi gerado
+})
